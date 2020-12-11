b6eb286013d3ccbef208ada1ef80be105089ac79
Rafael <rafaelpratasouza@gmail.com>
2020-12-10 21:50:29 -0300

    transformando o security em um gateway

--- provision/security.sh
+++ provision/security.sh
@@ -10,3 +10,11 @@ id devops
 if [ "$?" -ne 0 ]; then
     useradd -m -d /home/devops -s /bin/bash devops
 fi
+
+# Configurando como IP_FORWARD
+sysctl -w net.ipv4.ip_forward=1
+
+# Configurando o firewall
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE 
