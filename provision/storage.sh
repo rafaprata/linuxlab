d9a456bb96ba9e55f4585d583e5b9d1654e90fd4
Rafael <rafaelpratasouza@gmail.com>
2020-12-10 22:28:00 -0300

    Configurando o storage como gateway

--- provision/storage.sh
+++ provision/storage.sh
@@ -7,3 +7,12 @@ id devops
 if [ "$?" -ne 0 ]; then
     useradd -m -d /home/devops -s /bin/bash devops
 fi
+
+# Matando interface eth0
+echo " " > /etc/network/interfaces.d/eth0.cfg
+
+# Removendo Gateway pelo eth0
+route del default
+
+# Adicionando a máquina security como Gateway
+route add default gw 10.8.0.1 eth1
